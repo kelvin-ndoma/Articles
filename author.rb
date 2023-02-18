d62418ecd02d7144f788829a930def842888849b
kevin.mutua@student.moringaschool.com <kevin.mutua@student.moringaschool.com>
2023-02-18 14:32:39 +0300

    created a object relationshiop methods of author and magazine

--- author.rb
+++ author.rb
@@ -1,11 +1,26 @@
 class Author 
+
+    attr_reader :name
+
+    @all = []
+
     def initialize(name)
         @name = name
+        @@all << self
     end
 
-    def name
-        @name
+    def self.all
+        @@all
     end
+
+    def articles
+        Article.all.select {|article| article.author == self }
+    end
+
+    def magazines
+        self.articles.map {|article| article.magazine }.uniq
+    end
+
 end
 
 author = Author.new("Kelvin Ndoma")
